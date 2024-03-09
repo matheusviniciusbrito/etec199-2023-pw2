2836a12d66430ae68103eca4b7bbc69c0a491c30
Matheus Vinicius <78880370+matheusviniciusbrito@users.noreply.github.com>
2024-03-09 06:07:12 -0800

    Update ex4.js

--- Exercicios/ex4.js
+++ Exercicios/ex4.js
@@ -10,19 +10,19 @@ A cada passo, o programa deve imprimir o número atual da contagem. */
 inquirer
     .prompt([
         {
-        message: "Informe um número para a contagem: ",
-        name: "num",
-        validate: function(value) {
-            // Verifica se a entrada é um número inteiro positivo
-            var verificarpos = Math.sign(value);
-
-            if(verificarpos == 1){
-            // Se for um número inteiro, retorna true para passar a validação
-            return true;
-            } else{
-            // Se não for um número inteiro, retorna uma mensagem de erro
-            return 'Por favor, insira um número inteiro positivo. Não é necessario executar o programa novamente';
-            }
+            message: "Informe um número para a contagem: ",
+            name: "num",
+            validate: function(value) {
+                // Verifica se a entrada é um número inteiro positivo
+                var verificarpos = Math.sign(value);
+    
+                if(verificarpos == 1){
+                    // Se for um número inteiro, retorna true para passar a validação
+                    return true;
+                } else{
+                    // Se não for um número inteiro, retorna uma mensagem de erro
+                    return 'Por favor, insira um número inteiro positivo. Não é necessario executar o programa novamente';
+                }
         }
         }
     ])
